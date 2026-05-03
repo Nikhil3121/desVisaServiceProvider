75fae8807e955323c17c4b549e8cb246fcc5c160
nikhil3121 <singhnikhil3121@gmail.com>
2026-05-03 13:02:56 +0530

    newupate

--- src/components/Startproject.jsx
+++ src/components/Startproject.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+const API_URL = import.meta.env.VITE_API_URL || "https://desvisabackend.onrender.com";
 const SERVICES_ENDPOINT = `${API_URL}/api/services/`;
 const CREATE_ORDER_ENDPOINT = `${API_URL}/api/payments/create-order/`;
 const VERIFY_PAYMENT_ENDPOINT = `${API_URL}/api/payments/verify-payment/`;
